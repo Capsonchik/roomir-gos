58ec8caf29224acb07f7bfae3e654e8d7c9fda57
Khaustov.S <Khaustov.S@romir.ru>
2024-07-04 18:04:53 +0300

    Добавление чарта во второй блок

---
+++ src/components/charts/PurchasingActivityIndex.jsx
@@ -0,0 +1,72 @@
+import ReactECharts from "echarts-for-react";
+
+export const PurchasingActivityIndex = () => {
+  const option = {
+    // title: {
+    //   text: 'Stacked Line'
+    // },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['2016', '2017', '2018', '2019', '2020']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    // toolbox: {
+    //   feature: {
+    //     saveAsImage: {}
+    //   }
+    // },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: '2016',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 132, 101, 134, 90, 230, 210, 210, 220, 230, 240,250]
+      },
+      {
+        name: '2017',
+        type: 'line',
+        stack: 'Total',
+        data: [220, 182, 191, 234, 290, 330, 310, 320,330, 340, 250, 360]
+      },
+      {
+        name: '2018',
+        type: 'line',
+        stack: 'Total',
+        data: [150, 232, 201, 154, 190, 330, 410, 420, 430, 440, 450, 460]
+      },
+      {
+        name: '2019',
+        type: 'line',
+        stack: 'Total',
+        data: [320, 332, 301, 334, 390, 330, 320, 320, 330, 340, 350, 360]
+      },
+      {
+        name: '2020',
+        type: 'line',
+        stack: 'Total',
+        data: [820, 932, 901, 934, 1290, 1330, 1320, 1330, 1340,1350, 1360, 1370]
+      }
+    ]
+  };
+
+  return (
+    <>
+      <ReactECharts style={{height: '100%'}} option={option} />
+    </>
+  );
+};
--- src/components/materialSphere/materialSphere.jsx
+++ src/components/materialSphere/materialSphere.jsx
@@ -2,19 +2,27 @@ import styles from './styles.module.scss';
 import {SpendingRate} from "../charts/SpendingRate";
 import {PriceDinamyc} from "../priceDinamic/PriceDinamyc";
 import {Deflator} from "../charts/Deflator";
+import {PurchasingActivityIndex} from "../charts/PurchasingActivityIndex";
 export const MaterialSphere = () => {
   return (
     <div className={styles.container}>
       <div className={styles.topBlock}>
-        <div className={styles.first}>1</div>
-        <div className={styles.second}>2</div>
+        <div className={styles.first}>
+          <p>Индекс свободных денег</p>
+        </div>
+        <div className={styles.second}>
+          <p>Индекс покупательской активности</p>
+          <PurchasingActivityIndex/>
+        </div>
         <div className={styles.third}>
           <p>Доля трат</p>
           <SpendingRate/>
         </div>
       </div>
       <div className={styles.botBlock}>
-        <div className={styles.forth}>4</div>
+        <div className={styles.forth}>
+          <p>Динамика среднего чека</p>
+        </div>
         <div className={styles.fife}>
           <p>Дефлятор</p>
           <Deflator/>
