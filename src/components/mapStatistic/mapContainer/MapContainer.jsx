ead8b1b434c0a3bda8787a5cdf87a1f694af98c3
capsonchik <serega00790@mail.ru>
2024-07-06 15:14:31 +0300

    Карта

--- src/components/mapStatistic/mapContainer/MapContainer.jsx
+++ src/components/mapStatistic/mapContainer/MapContainer.jsx
@@ -2,11 +2,12 @@ import styles from './styles.module.scss';
 import {MapDropDown} from "../mapComponents/mapDropDown/MapDropDown";
 import {CityItem} from "../mapComponents/cityItem/CityItem";
 import {REGION} from "../../consts/filterConsts";
+import {Map} from "../mapComponents/map/Map";
 export const MapContainer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.map}>
-
+        <Map/>
       </div>
       <div className={styles.stat}>
         <MapDropDown/>
