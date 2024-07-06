d45e21205ce0ea44f1cb2b6ce1bd715527181ac5
capsonchik <serega00790@mail.ru>
2024-07-06 18:51:21 +0300

    Индекс активности

---
+++ src/components/charts/ConsumerActivityIndex.jsx
@@ -0,0 +1,71 @@
+import ReactECharts from "echarts-for-react";
+
+export const ConsumerActivityIndex = () => {
+  const option = {
+    title: {
+      text: 'Индекс потребительской уверенности',
+      left: 'center',
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: [
+          'Октябрь', 'Ноябрь', 'Декабрь', 'Январь', 'Февраль', 'Март', 'Апрель',
+          'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь',
+          'Декабрь', 'Январь', 'Февраль'
+        ],
+        axisLabel: {
+          rotate: 45,
+        },
+      },
+      {
+        type: 'category',
+        position: 'top',
+        offset: 30,
+        data: [
+          '', '', '', '2022', '', '', '', '',
+          '', '', '', '', '2023', '', '', '', '', '2024'
+        ],
+        axisTick: {
+          alignWithLabel: false,
+          length: 0,
+        },
+        axisLine: {
+          show: false,
+        },
+      }
+    ],
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Индекс',
+        type: 'bar',
+        data: [
+          -12, -8, -7, -1, -1, 1, 1, 1, 1, -1, 0, -4, -2, 1, 1, 2, 5, 11
+        ],
+        itemStyle: {
+          color: function (params) {
+            return params.value > 0 ? '#76C043' : '#E83132';
+          },
+        },
+      },
+    ],
+  };
+  return (
+    <ReactECharts style={{height: 700}} option={option} />
+  );
+};
